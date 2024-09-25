3baed3359b8617953f1624d9e0c642372b3a8539
JAIS_PC\jaivd <jaivda111@gmail.com>
2024-09-26 00:52:33 +1000

    Final small tweaks of the night

---
+++ Assets/Gear.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gear : MonoBehaviour
+{
+    //dropdown - Type(helmet)
+    //Dropdown - Teir(1)
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
