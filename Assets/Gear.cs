8c6ad8b499cd9fe6779b3e469252354504b2f327
JAIS_PC\jaivd <jaivda111@gmail.com>
2024-09-30 12:23:29 +1000

    Sum Tings (i dont remember)

--- Assets/Gear.cs
+++ Assets/Gear.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Gear : MonoBehaviour
 {
-    //dropdown - Type(helmet)
+    //dropdown - GearType(helmet)
     //Dropdown - Teir(1)
+
+    public GearType type = new GearType();
+    public GearTeir teir = new GearTeir();
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -17,4 +19,22 @@ public class Gear : MonoBehaviour
     {
         
     }
+    public enum GearType
+    {
+        Helmet,
+        Chestplate,
+        Leggings,
+        Gauntlet,
+        Boots,
+        Weapon
+    };
+
+    public enum GearTeir
+    {
+        One,
+        Two,
+        Three,
+        Four,
+        Five
+    };
 }
