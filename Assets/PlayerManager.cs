f5583728258577713a59cad7ac8e1313852f6b93
JAIS_PC\jaivd <jaivda111@gmail.com>
2024-11-20 22:46:20 +1000

    Added Login + Username Set

--- Assets/PlayerManager.cs
+++ Assets/PlayerManager.cs
@@ -2,17 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using LootLocker.Requests;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PlayerManager : MonoBehaviour
 {
+    public string startScene;
+    public TMP_InputField username;
     // Start is called before the first frame update
     void Start()
     {
+        if (PlayerPrefs.GetString("PlayerName") != "")
+        {
+            username.text = PlayerPrefs.GetString("PlayerName");
+        }
+    }
+
+    public void LoginGuest()
+    {
         StartCoroutine(LoginRoutine());
-        
     }
 
     IEnumerator LoginRoutine()
+    {
+        yield return LoginRoutineGuest();
+        yield return SetPlayerNameRoutine(); 
+        SceneManager.LoadSceneAsync(startScene);
+    }
+
+    IEnumerator SetPlayerNameRoutine()
+    {
+        if (username.text != "")
+        {
+            bool done = false;
+            LootLockerSDKManager.SetPlayerName(username.text, (response) =>
+            {
+                if (response.success)
+                {
+                    print("Set player name!");
+                    done = true;
+                    PlayerPrefs.SetString("PlayerName", username.text);
+                }
+                else
+                {
+                    Debug.LogError("Failed to set player name! " + response.errorData);
+                    done = true;
+                }
+            });
+            while (!done)
+            {
+                yield return null;
+            }
+            yield return true;
+        }
+        else
+        {
+            yield return null;
+        }
+    }
+
+    IEnumerator LoginRoutineGuest()
     {
         bool done = false;
         LootLockerSDKManager.StartGuestSession((response) =>
@@ -25,12 +74,17 @@ public class PlayerManager : MonoBehaviour
             }
             else
             {
-                Debug.LogError("Player Login Failed! "+ response.errorData);
+                Debug.LogError("Player Login Failed! " + response.errorData);
                 done = true;
             }
         });
 
-        yield return new WaitWhile(()=>done = false);
+        //yield return new WaitWhile(() => done = false);
+        while (!done)
+        {
+            yield return null;
+        }
+        yield return true;
     }
 
     // Update is called once per frame
@@ -38,11 +92,11 @@ public class PlayerManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            StartCoroutine(FindObjectOfType<Leaderboard>().SubmitScoreRoutine(882));
+            //StartCoroutine(FindObjectOfType<Leaderboard>().SubmitScoreRoutine(882));
         }
         if (Input.GetKeyDown(KeyCode.W))
         {
-            StartCoroutine(FindObjectOfType<Leaderboard>().FetchTopHighScoresRountine());
+            //StartCoroutine(FindObjectOfType<Leaderboard>().FetchTopHighScoresRountine());
         }
     }
 }
